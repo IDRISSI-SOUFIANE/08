54f4e87f1152fc90204335938c72801cc49a7ac0
IDRISSI-SOUFIANE <idrissisoufiane2000@gmail.com>
2025-12-25 12:52:53 +0100

    08

---
+++ ex000/easyfind.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sidrissi <sidrissi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/23 10:17:43 by sidrissi          #+#    #+#             */
+/*   Updated: 2025/12/25 12:18:15 by sidrissi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+template <typename T>
+void	easyfind(T& container, int value)
+{
+	for (int i = 0; i < container.size(); i++)
+	{
+		if (container[i] == value)
+		{
+			std::cout << "value found\n";
+			return ;
+		}
+	}
+	std::cout << "value not found\n";
+}
+
+#endif
